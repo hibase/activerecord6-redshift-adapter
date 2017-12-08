ddca88a99a2a1bb338f91122c6276402e4a8b5d4
xueyong zhi <xue.yong.zhi@gmail.com>
2017-12-08 16:55:07 -0500

    fix integer migration in rails 5.1

--- lib/active_record/connection_adapters/redshift/schema_statements.rb
+++ lib/active_record/connection_adapters/redshift/schema_statements.rb
@@ -372,7 +372,7 @@ module ActiveRecord
         end
 
         # Maps logical Rails types to PostgreSQL-specific data types.
-        def type_to_sql(type, limit = nil, precision = nil, scale = nil)
+        def type_to_sql(type, limit = nil, precision = nil, scale = nil, **)
           case type.to_s
           when 'integer'
             return 'integer' unless limit
